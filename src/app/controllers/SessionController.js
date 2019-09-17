b6b724a98be62c8b7d96f85a17b2db8afd51c622
Zelp <tocchettoo@gmail.com>
2019-09-17 17:59:34 -0300

    Adicionada validacao de atuenticacao e troca de senha

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,9 +1,21 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import User from '../models/User';
+import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
@@ -24,8 +36,8 @@ class SessionController {
         name,
         email,
       },
-      token: jwt.sign({ id }, '9b59a27699da1e6f8d9db3b4447d2cdd', {
-        expiresIn: '7d',
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
       }),
     });
   }
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '9b59a27699da1e6f8d9db3b4447d2cdd',
+  expiresIn: '7d',
+};
